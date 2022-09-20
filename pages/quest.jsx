d72078d8b2faa6775a3f29cbcf44c9a4a6fee6f6
Lara <lara.dashut@gmail.com>
2022-09-20 15:42:00 -0700

    draft

---
+++ pages/quest.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+const Quest = () => {
+  return (
+    <div className={styles.grid}>
+      <h2>Here is the quest list:</h2>
+    </div>
+  );
+};
+
+export default Quest;
